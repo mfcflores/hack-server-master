1f47604e3ea604f7901f674d70c7f50c0be21e05
moylua <moylua@gmail.com>
2013-11-23 23:16:07 +0800

    modified userservice

--- protected/components/UserService.php
+++ protected/components/UserService.php
@@ -1,49 +1,49 @@
 <?php
 
-Class UserService extends CComponent 
-{
-	function __construct() 
-	{
+Class UserService extends CComponent {
 
-	}
-	/*
-	* This method gets location of user.
-	* @param user_id
-	* @return $x, $y & $datetime
-	*/
-	public function getLocation($get)
-	{
-		if(!array_key_exists('user_id', $get)) 
-		{
-			return array('success' => false, 'message' => 'Invalid Parameters');
-		}
+    function __construct() {
+        
+    }
 
-		$res = EDMSQuery::instance('user_location')->findOne(array('_id' => new MongoId($get['user_id'])));
-		
-		return $res;
-	}
-	
-	/*
-	* This method sets the location of user.
-	* @param string $user_id
-	* @param integer $x
-	* @param integer $y
-	* @return boolean true or false
-	*/
-	public function sendLocation($user_id, $latitude, $longitude)
-	{
-		$location = array( 
-			'user_id' 	=> $user_id, 
-			'longitude'	=> $longitude,
-			'latitude'	=> $latitude
-		);
-		
-		$res 	= EDMSQuery::instance('services')->insert($location);
-	}
-    
+    /*
+     * This method gets location of user.
+     * @param user_id
+     * @return $x, $y & $datetime
+     */
+
+    public function getLocation($get) {
+        if (!array_key_exists('user_id', $get)) {
+            return array('success' => false, 'message' => 'Invalid Parameters');
+        }
+
+        $res = EDMSQuery::instance('user_location')->findOne(array('_id' => new MongoId($get['user_id'])));
+
+        return $res;
+    }
+
+    /*
+     * This method sets the location of user.
+     * @param string $user_id
+     * @param integer $x
+     * @param integer $y
+     * @return boolean true or false
+     */
+
+    public function sendLocation($user_id, $latitude, $longitude) {
+        $location = array(
+            'user_id' => $user_id,
+            'longitude' => $longitude,
+            'latitude' => $latitude
+        );
+
+        $res = EDMSQuery::instance('services')->insert($location);
+    }
+
     public function syncData() {
         $res = EDMSQuery::instance('vehicles')->findArray();
     }
+
 }
 
 ?>
