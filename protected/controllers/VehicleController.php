66b90165ecf39a868a1c0988681392fd92265cfb
moylua <moylua@gmail.com>
2013-11-23 16:40:21 +0800

    added restfulyii
    edit gitgnore

---
+++ protected/controllers/VehicleController.php
@@ -0,0 +1,26 @@
+<?php
+
+class VehicleController extends ERestController {
+
+    /**
+     * Declares class-based actions.
+     */
+    public function actions() {
+        return array(
+        );
+    }
+
+    /**
+     * This is the default 'index' action that is invoked
+     * when an action is not explicitly requested by users.
+     */
+    public function actionIndex() {
+        // renders the view file 'protected/views/site/index.php'
+        // using the default layout 'protected/views/layouts/main.php'
+        $this->render('index');
+    }
+
+    public function doCustomRestGetGetLocation() {
+        echo CJSON::encode(array('value' => 'test'));
+    }
+}
